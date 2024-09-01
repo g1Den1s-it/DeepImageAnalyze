55e203b405625792413ffe178d25d290f43097a7
g1Den1s-it <g1den1splay@gmail.com>
2024-09-01 16:29:25 +0300

    added alembic

--- src/main.py
+++ src/main.py
@@ -6,10 +6,5 @@ from database import Base, engine
 app = FastAPI()
 
 
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
-
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
