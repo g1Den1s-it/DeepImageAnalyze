6f6bb5377fcd011e731bef0a4c389cdf3b6525bb
g1Den1s-it <g1den1splay@gmail.com>
2024-10-06 17:14:10 +0300

    Implemented route for accessing media static files.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from src.auth.router import auth
+from src.config import static_base_config
 from src.report.router import report_router
 
 app = FastAPI()
 
 app.include_router(auth)
+app.mount("/media", StaticFiles(directory=static_base_config.MEDIA_NAME), name=static_base_config.MEDIA_NAME)
 app.include_router(report_router)
 
 if __name__ == "__main__":
